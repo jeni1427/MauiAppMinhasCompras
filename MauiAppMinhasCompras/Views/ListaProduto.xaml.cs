cea9101b70b8abd560da0fdd09f4b0e604b0e71f
jenifer lima de jesus <jenifer.jesus3@etec.sp.gov.br>
2025-03-12 21:03:00 -0300

    alteração nas pastas ListaProduto e SQliteDataBase e incrementação do "FROM" na pasta produtos.

--- MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,9 +1,47 @@
+using System.Collections.ObjectModel;
+using MauiAppMinhasCompras.Models;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
+
 {
-	public ListaProduto()
-	{
-		InitializeComponent();
-	}
+    ObservableCollection<Produto> Lista = new ObservableCollection<Produto>();
+    public ListaProduto()
+    {
+        InitializeComponent();
+
+        lst_produtos.ItemsSource = Lista;
+    }
+    protected async override void OnAppearing()
+    {
+        List<Produto> tmp = await App.Db.GetAll();
+
+        tmp.ForEach(i => Lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        Lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => Lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+        double soma = Lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "Ok");
+    }
 }
